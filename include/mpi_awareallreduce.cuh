c91e66c5ccea1885fd38f8493c908af4593054d6
Francesco Carboni <121803863+cornflexxx@users.noreply.github.com>
2025-07-18 12:31:34 +0200

    header

--- include/mpi_awareallreduce.cuh
+++ include/mpi_awareallreduce.cuh
@@ -5,3 +5,8 @@ int allreduce_ring_comprs_hom_sum(const float *d_sbuf, float *d_rbuf,
                                   size_t count, MPI_Comm comm, float eb);
 int allreduce_ring_comprs_hom_sum_F(const float *d_sbuf, float *d_rbuf,
                                     size_t count, MPI_Comm comm, float eb);
+
+int allreduce_ring_comprs_hom_sum_seg(const float *d_sbuf, float *d_rbuf,
+                                      size_t count, MPI_Comm comm, float eb);
+int allreduce_ring_comprs_hom_sum_F_seg(const float *d_sbuf, float *d_rbuf,
+                                        size_t count, MPI_Comm comm, float eb);
--- include/nca_allreduce.cuh
+++ include/nca_allreduce.cuh
@@ -6,3 +6,10 @@ int cpuCopy_allreduce_ring_comprs_hom_sum(const float *d_sbuf, float *d_rbuf,
 int cpuCopy_allreduce_ring_comprs_hom_sum_F(const float *d_sbuf, float *d_rbuf,
                                             size_t count, MPI_Comm comm,
                                             float eb);
+
+int cpuCopy_allreduce_ring_comprs_hom_sum_F_seg(const float *d_sbuf,
+                                                float *d_rbuf, size_t count,
+                                                MPI_Comm comm, float eb);
+int cpuCopy_allreduce_ring_comprs_hom_sum_seg(const float *d_sbuf,
+                                              float *d_rbuf, size_t count,
+                                              MPI_Comm comm, float eb);
